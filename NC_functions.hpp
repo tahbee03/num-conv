2dc17817fdba1bbe6b2692ce1fa8d5908ba9b0e5
Tah <65676639+TahBee03@users.noreply.github.com>
2022-01-03 16:36:22 -0500

    Updated NC_functions.hpp

--- NC_functions.hpp
+++ NC_functions.hpp
@@ -4,99 +4,121 @@ PROJECT: Number Converter
 FILE: NC_functions.hpp
 */
 
-#ifndef HM2_FUNC
-#define HM2_FUNC
+#ifndef NC_FUNC
+#define NC_FUNC
 
-#include <cmath>
-#include <string> // Library needed to use string operations.
+#include <cmath> // pow()
+#include <string> // string
 using namespace std;
 
-string BasetoDec(string source, int baseS) {
+string BaseToDec(string source, int baseS) {
 
     /* 
-    BASE(2-9)-TO-DECIMAL CONVERSION.
-    Based on Demo 4.15.
-    Modified to work for numbers of base 2 to base 9. 
+    BASE(2-9)-TO-DECIMAL CONVERSION
+    Based on Demo 4.15
+    Modified to work for numbers of base 2 to base 9 
     */
 
     string target = "";
-    int digit = 0;
-    int temp = 0;
+    int digit;
+    int target_int = 0;
 
     for(int i = 0; i < source.size(); i++) {
 
-        digit = source[i] - '0'; // Converts each string digit to an integer digit.
-        temp += digit * pow(baseS, source.size() - (i + 1));
+        digit = source[i] - '0';
+        target_int += digit * pow(baseS, source.size() - (i + 1));
 
     }
 
-    target = to_string(temp);
+    target = to_string(target_int);
     return target;
 
 }
 
-string DectoBase(string source, int baseT) {
+string HexToDec(string source) {
 
     /* 
-    DECIMAL-TO-BASE(2-9) CONVERSION.
-    Based on in-class presentation (Mar 3) and Demo 4.15.
-    Modified to work for (1) numbers of base 2 to base 9, and (2) decimals input as strings. 
+    HEXADECIMAL-TO-DECIMAL CONVERSION
+    Based on Demo 4.15
     */
 
     string target = "";
-    string temp = "";
-    const int decimalBase = 10; // Base value of decimals.
-    int decimalInt = 0; // Stores the value of sourceNum as an integer.
+    const int hexBase = 16; // Base value of hexadecimals
+    int digit;
+    int target_int = 0;
 
-    decimalInt = stoi(source);
+    for(int i = 0; i < source.size(); i++) {
 
-    while(decimalInt > 0) {
+        if(source.at(i) >= 'a' && source.at(i) <= 'f') digit = source.at(i) - 'a' + 10;
+        else if(source.at(i) >= 'A' && source.at(i) <= 'F') digit = source.at(i) - 'A' + 10;
+        else digit = source.at(i) - '0';
 
-        temp += to_string(decimalInt % baseT);
-        decimalInt /= baseT;
+        target_int += digit * pow(hexBase, source.size() - (i + 1));
 
     }
 
-    for(int i = temp.size() - 1; i >= 0; i--) target += temp[i]; // Reverses the string to print the characters in the right order.
-
+    target = to_string(target_int);
     return target;
 
 }
 
-string DectoHex(string source) {
+string DecToBase(string source, int baseT) {
 
     /* 
-    DECIMAL-TO-HEXADECIMAL CONVERSION.
-    Based on in-class presentation (Mar 3) and Demo 4.15.
-    Modified to convert remainder values greater than 9 to their corresponding hexadecimal letter. 
+    DECIMAL-TO-BASE(2-9) CONVERSION
+    Based on in-class presentation (Mar 3, 2021) and Demo 4.15
+    Modified to work for (1) numbers of base 2 to base 9, and (2) decimals input as strings
     */
 
     string target = "";
-    string temp = "";
-    const int hexBase = 16;
-    int decimalInt = 0;
+    string target_temp = "";
+    const int decimalBase = 10; // Base value of decimals
+    int source_int = 0; // Stores the value of source as an integer
 
-    decimalInt = stoi(source);
+    source_int = stoi(source);
 
-    while(decimalInt > 0) {
+    while(source_int > 0) {
 
-        int rem = decimalInt % hexBase; // Holds the result of decimalInt % targetBase.
-
-        if(rem <= 9) temp += rem + '0'; // Stores a digit based on the ASCII value derived from rem + '0'.
-        else temp += rem - 10 + 'A'; // Stores a letter based on the ASCII value derived rem - 10 + 'A'. Inverse of letter-to-number conversion from Demo 4.15 (letter - 'A' + 10).
-        decimalInt /= hexBase;
+        target_temp += to_string(source_int % baseT);
+        source_int /= baseT;
 
     }
 
-    temp += "x0"; // Hexadecimals are usually prepended with '0x'.
-    for(int i = temp.size() - 1; i >= 0; i--) target += temp[i]; // Reverses the string to print the characters in the right order.
-
+    for(int i = target_temp.size() - 1; i >= 0; i--) target += target_temp[i]; // Reverses the string to print the characters in the right order
     return target;
 
 }
 
-string HextoOct(string source); // FIX
+string DecToHex(string source) {
 
-string HextoDec(string source); // FIX
+    /* 
+    DECIMAL-TO-HEXADECIMAL CONVERSION
+    Based on in-class presentation (Mar 3) and Demo 4.15
+    Modified to convert remainder values greater than 9 to their corresponding hexadecimal letter
+    */
+
+    string target = "";
+    string target_temp = "";
+    const int hexBase = 16; // Base value of hexadecimals
+    int source_int = 0; // Stores the value of source as an integer
+
+    source_int = stoi(source);
+
+    while(source_int > 0) {
+
+        int rem = source_int % hexBase;
+
+        if(rem <= 9) target_temp += rem + '0'; // Stores a digit based on the ASCII value derived from rem + '0'
+        else target_temp += rem - 10 + 'A'; // Stores a letter based on the ASCII value derived rem - 10 + 'A'
+        source_int /= hexBase;
+
+    }
+
+    target_temp += "x0"; // Hexadecimals are usually prepended with '0x'
+    for(int i = target_temp.size() - 1; i >= 0; i--) target += target_temp[i]; // Reverses the string to print the characters in the right order
+
+    return target;
+
+}
 
 #endif
